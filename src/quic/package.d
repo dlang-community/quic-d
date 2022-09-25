9321345249ee79339cf04b37a770771ae803085b
Petar Kirov <petar.p.kirov@gmail.com>
2022-09-26 01:09:34 +0300

    test(deimos.openssl): Test that OpenSSL's version is >= 3.0.5

--- src/quic/package.d
+++ src/quic/package.d
@@ -1,5 +1,11 @@
 module quic;
 
+unittest
+{
+    import deimos.openssl.opensslv : OPENSSL_VERSION_AT_LEAST;
+    static assert(OPENSSL_VERSION_AT_LEAST(3, 0, 5));
+}
+
 int test(bool cond)
 {
     if (cond)
