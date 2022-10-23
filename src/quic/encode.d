19d2b484258b01f9dd1a833253fd68f075813f0b
vladchicos <vladut.chicos@gmail.com>
2022-10-23 13:02:42 +0300

    Add VarInt encode unittest Fix encode module

--- src/quic/encode.d
+++ src/quic/encode.d
@@ -2,25 +2,57 @@ module quic.encode;
 
 import quic.frame : VarInt;
 
-void encodeVarInt(Writer)(ref Writer writer, VarInt value)
+ubyte getVarIntBitMask(ubyte len)
 {
-    ubyte len;
-
-    if(value <= ulong.max)
-        len = ulong.sizeof;
-    if(value <= uint.max)
-        len = uint.sizeof;
-    if(value <= ushort.max)
-        len = ushort.sizeof;
-    if(value <= ubyte.max)
-        len = ubyte.sizeof;
-
-    for(ubyte i=0; i<len; i++)
+    uint bitMask;
+    while(len > 1)
     {
-        if(i == 0)
-            writer ~= value & 0xff & len;
-        else
-            writer ~= value & 0xff;
-        value = value >>> 8;
+        bitMask++; 
+        len = len >>> 1; 
     }
+    return cast(ubyte)((bitMask << 6));
+}
+
+void encodeVarInt(Writer)(ref Writer writer, VarInt value)
+in {
+    assert(value < (1UL << 62), "Value is too big");
+} do {
+    ubyte len;
+    if (value < 1UL << 6)
+        len = 1;
+    else if (value < 1UL << 14)
+        len = 2;
+    else if (value < 1UL << 30)
+        len = 4;
+    else if (value < 1UL << 62)
+        len = 8;
+
+    import std.bitmanip : nativeToBigEndian;
+    auto bigEndianValue = nativeToBigEndian(value);
+
+    foreach (i, elem; bigEndianValue[$-len..$])
+    {
+        if (i == 0)
+            writer ~= elem | getVarIntBitMask(len);
+        else
+            writer ~= elem;
+    }
+}
+
+unittest
+{
+    import std.conv : hexString;
+    
+	ubyte[] testBuf = cast(ubyte[]) hexString!"c2197c5eff14e88c";
+    testBuf ~= cast(ubyte[]) hexString!"9d7f3e7d";
+    testBuf ~= cast(ubyte[]) hexString!"7bbd";
+    testBuf ~= cast(ubyte[]) hexString!"25";
+
+	ubyte[] buf;
+	encodeVarInt(buf, 151288809941952652);
+	encodeVarInt(buf, 494878333);
+	encodeVarInt(buf, 15293);
+	encodeVarInt(buf, 37);
+
+	assert(testBuf == buf);
 }
